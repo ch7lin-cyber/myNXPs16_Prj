46e1db42784f744be1129e00fae7b9c4f78c7e10
ch7lin-cyber <ch7.lin@gmail.com>
2026-09-22 16:21:45 +0800

    Update PRODUCT_TC_R_TABLE_COMPLETE and measurement counts

--- _Product/Source/L03_System/SensorTables/product_tc_r_table.h
+++ _Product/Source/L03_System/SensorTables/product_tc_r_table.h
@@ -7,16 +7,16 @@
 extern "C" {
 #endif
 
-#define PRODUCT_TC_R_TABLE_COMPLETE                (0U)
+#define PRODUCT_TC_R_TABLE_COMPLETE                (1U)
 #define PRODUCT_TC_R_MIN_MILLICELSIUS              (0L)
 #define PRODUCT_TC_R_MAX_MILLICELSIUS              (1700000L)
 #define PRODUCT_TC_R_EXTENDED_MARGIN_MC            (20000L)
 
 /* TODO: Enter this sensor's measurement-table parameters. */
-#define PRODUCT_TC_R_MEASUREMENT_SEGMENT_COUNT     (0U)
+#define PRODUCT_TC_R_MEASUREMENT_SEGMENT_COUNT     (87U)
 #define PRODUCT_TC_R_MEASUREMENT_BOUNDARY_COUNT    \
     (PRODUCT_TC_R_MEASUREMENT_SEGMENT_COUNT + 1U)
-#define PRODUCT_TC_R_INPUT_SHIFT_UV                (0L)
+#define PRODUCT_TC_R_INPUT_SHIFT_UV                (110L)
 #define PRODUCT_TC_R_MEASUREMENT_COEFFICIENT_SCALE (100UL)
 
 /* CJC covers -20.0 to 109.0 degree C in 10 degree C groups. */
