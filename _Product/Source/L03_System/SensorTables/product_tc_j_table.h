9f750b2603c10692b756aa9abd21b45b9414bdde
ch7lin-cyber <ch7.lin@gmail.com>
2026-09-22 16:22:33 +0800

    Update product_tc_j_table.h

--- _Product/Source/L03_System/SensorTables/product_tc_j_table.h
+++ _Product/Source/L03_System/SensorTables/product_tc_j_table.h
@@ -7,7 +7,7 @@
 extern "C" {
 #endif
 
-#define PRODUCT_TC_J_TABLE_COMPLETE                (0U)
+#define PRODUCT_TC_J_TABLE_COMPLETE                (1U)
 #define PRODUCT_TC_J_MIN_MILLICELSIUS              (-200000L)
 #define PRODUCT_TC_J_MAX_MILLICELSIUS              (1200000L)
 #define PRODUCT_TC_J_EXTENDED_MARGIN_MC            (20000L)
