4964c5200cbb5350e9e5177ed24f44ec2affdcfe
ch7lin-cyber <ch7.lin@gmail.com>
2026-09-22 16:22:05 +0800

    Change PRODUCT_TC_L_TABLE_COMPLETE value to 1

--- _Product/Source/L03_System/SensorTables/product_tc_l_table.h
+++ _Product/Source/L03_System/SensorTables/product_tc_l_table.h
@@ -7,7 +7,7 @@
 extern "C" {
 #endif
 
-#define PRODUCT_TC_L_TABLE_COMPLETE                (0U)
+#define PRODUCT_TC_L_TABLE_COMPLETE                (1U)
 #define PRODUCT_TC_L_MIN_MILLICELSIUS              (-200000L)
 #define PRODUCT_TC_L_MAX_MILLICELSIUS              (850000L)
 #define PRODUCT_TC_L_EXTENDED_MARGIN_MC            (20000L)
