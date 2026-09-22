62d9484d4dea0a997c3734ea404eaeb4fff05e86
ch7lin-cyber <ch7.lin@gmail.com>
2026-09-22 16:03:39 +0800

    Update measurement segment count and input shift values

--- _Product/Source/L03_System/SensorTables/product_tc_j_table.h
+++ _Product/Source/L03_System/SensorTables/product_tc_j_table.h
@@ -13,10 +13,10 @@ extern "C" {
 #define PRODUCT_TC_J_EXTENDED_MARGIN_MC            (20000L)
 
 /* TODO: Enter this sensor's measurement-table parameters. */
-#define PRODUCT_TC_J_MEASUREMENT_SEGMENT_COUNT     (0U)
+#define PRODUCT_TC_J_MEASUREMENT_SEGMENT_COUNT     (70U)
 #define PRODUCT_TC_J_MEASUREMENT_BOUNDARY_COUNT    \
     (PRODUCT_TC_J_MEASUREMENT_SEGMENT_COUNT + 1U)
-#define PRODUCT_TC_J_INPUT_SHIFT_UV                (0L)
+#define PRODUCT_TC_J_INPUT_SHIFT_UV                (7900L)
 #define PRODUCT_TC_J_MEASUREMENT_COEFFICIENT_SCALE (100UL)
 
 /* CJC covers -20.0 to 109.0 degree C in 10 degree C groups. */
